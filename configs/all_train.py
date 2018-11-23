c2f2872bf0201b6b11855815af6b54ad3f6806d8
Rohit Prasad <rohitrp3@gmail.com>
2018-11-24 04:57:23 +0530

    Add configs for transfer learning

---
+++ configs/all_train.py
@@ -0,0 +1,9 @@
+class Config:
+    def __init__(self):
+        self.conv1_train = True
+        self.conv2_train = True
+        self.conv3_train = True
+        self.conv4_train = True
+        self.lstm_train = True
+        self.critic_linear_train = True
+        self.actor_linear_train = True
---
+++ configs/train_conv4.py
@@ -0,0 +1,9 @@
+class Config:
+    def __init__(self):
+        self.conv1_train = False
+        self.conv2_train = False
+        self.conv3_train = False
+        self.conv4_train = True
+        self.lstm_train = True
+        self.critic_linear_train = True
+        self.actor_linear_train = True
