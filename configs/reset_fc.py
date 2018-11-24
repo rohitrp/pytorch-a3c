92b27a4cf2140f07ed2427a0d7ef76a6d5f0fdf1
Rohit Prasad <rohitrp3@gmail.com>
2018-11-24 07:16:43 +0530

    Add new config

---
+++ configs/reset_fc.py
@@ -0,0 +1,17 @@
+class Config:
+    def __init__(self):
+        self.conv1_train = True
+        self.conv2_train = True
+        self.conv3_train = True
+        self.conv4_train = True
+        self.lstm_train = True
+        self.critic_linear_train = True
+        self.actor_linear_train = True
+
+        self.conv1_reset = False
+        self.conv2_reset = False
+        self.conv3_reset = False
+        self.conv4_reset = False
+        self.lstm_reset = False
+        self.critic_linear_reset = True
+        self.actor_linear_reset = True
